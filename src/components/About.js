a67c4b2e726a353cb59f6210032cb97797f8f26c
Mason Howe <mason.a.howe@gmail.com>
2021-04-03 17:03:13 -0600

    add to work section

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function About() {
   return (
     <div>
-      <p>Here is the about me page: It will do some about me things!</p>
+      <h1>Here is the about me page: It will do some about me things!</h1>
     </div>
   );
 }
--- src/components/Work.js
+++ src/components/Work.js
@@ -4,6 +4,20 @@ export default function Work() {
   return (
     <div>
       <h1>Example of work</h1>
+      <br></br>
+      <div className="What-To-Watch">
+        <p>What to Watch front end</p>
+        <iframe
+          src="https://kolbylaw.github.io/What-to-Watch/"
+          className="What-To-Watch-Iframe"
+        ></iframe>
+      </div>
+      <div className="Portfolio-Creator">
+        <iframe
+          src="https://blooming-peak-48470.herokuapp.com/"
+          className="Portfolio-Creator-Iframe"
+        ></iframe>
+      </div>
     </div>
   );
 }
