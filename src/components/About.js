2264c1a64d2adbedc5f8de9ff6f81d91dbc120e3
Mason Howe <mason.a.howe@gmail.com>
2021-04-04 11:59:00 -0600

    Add contact info

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,23 @@
 import React from "react";
+import AboutMe from "../assets/Images/AboutImages/0.jpg";
 
 export default function About() {
   return (
     <div>
-      <h1>Here is the about me page: It will do some about me things!</h1>
+      <div className="about-me-header" id="about-me">
+        <div className="about-me-flex">
+          <h1>About Me!</h1>
+          <img src={AboutMe} className="about-me-img"></img>
+        </div>
+      </div>
+      <div className="about-me-text">
+        <p>
+          My Name is Mason Howe I was born on Salt Lake City Utah.<br></br> I
+          currently live in West Valley City Utah. I graduated From Salt Lake
+          Community College in 2015 I am currently a student at the University
+          of Utah.
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,9 @@ import Hero from "./Heroimage";
 export default function Home() {
   return (
     <div>
-      <Hero />
       <h1>Welcome to the Mason's Portfolio!</h1>
-      <p>You can use the About section to read more about me.</p>
-      <p>
-        You can use the work section to see examples of projects that I been
-        worked on.
-      </p>
-      <p>You can use the Contact Page to find my contact infomation.</p>
+      <p>Use the navigation tabs at the top to navigate to the various pages</p>
+      <Hero />
     </div>
   );
 }
